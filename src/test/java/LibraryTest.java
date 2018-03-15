61c1ac3f8ac059f8269e88cc4189b68f9eb8234f
Sergey Lobin <ifitwasi@gmail.com>
2018-03-15 14:46:40 +0300

    java project init with one test

---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StringCalculatorTest {
+
+    @Test
+    void test() {
+        assertEquals(1, 1);
+    }
+}
